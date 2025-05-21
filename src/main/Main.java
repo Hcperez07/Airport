1b49266d094e274af93d1f5bf73ef0f4e5dafe00
Carlos Pérez <hcperez@uninorte.edu.co>
2025-05-21 10:45:42 -0500

    Primer commit, se creo un main

---
+++ src/main/Main.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author carha
+ */
+import com.formdev.flatlaf.FlatDarkLaf;
+import javax.swing.UIManager;
+
+import airport.AirportFrame;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.setProperty("flatlaf.useNativeLibrary", "false");
+        try {
+            UIManager.setLookAndFeel(new FlatDarkLaf());
+        } catch (Exception ex) {
+            System.err.println("Failed to initialize LaF");
+        }
+
+        java.awt.EventQueue.invokeLater(() -> {
+            new AirportFrame().setVisible(true);
+        });
+    }
+}
